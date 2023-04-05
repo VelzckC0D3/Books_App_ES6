3f73688b13b3bde829b38781be83192ccd4f78eb
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-04-04 22:37:37 -0500

    Update Luxon
    - Update Luxon so it works properly

--- modules/date.js
+++ modules/date.js
@@ -1,4 +1,4 @@
-import { DateTime } from '../node_modules/luxon/src/luxon.js';
+import { DateTime } from '../luxon/src/luxon.js';
 
 const getFormattedDateTime = () => {
   const dt = DateTime.now();
